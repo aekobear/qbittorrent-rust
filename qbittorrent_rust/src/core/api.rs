cbb1b7cff848c6d80b17cbd8fedfaa0383dccc1b
aekobear <aekobear@gmail.com>
2025-08-13 19:51:13 -0400

    Remove printlns

--- qbittorrent_rust/src/core/api.rs
+++ qbittorrent_rust/src/core/api.rs
@@ -1,21 +1,23 @@
-use std::{collections::HashMap, hash::Hash, sync::Arc};
-#[allow(unused_imports)]
-use serde_json::Value;
 use reqwest::{header::COOKIE, Client};
 use serde::Serialize;
+#[allow(unused_imports)]
+use serde_json::Value;
+use std::{collections::HashMap, hash::Hash, sync::Arc};
 use tokio::sync::{Mutex, RwLock};
 
-use crate::{code, core::cookie::Cookie, error_handling::error_type::ErrorType, post_request_no_return};
+use crate::{
+    code, core::cookie::Cookie, error_handling::error_type::ErrorType, post_request_no_return,
+};
 
 use super::creds::Credentials;
 use crate::error_handling::errors::Error;
 
 ///## Description
 /// the main struct of the library.
-/// each API-related method is listed as its category, followed by its name: `category` + `name_of_the_method`, in snake case. (eg: `torrents_add_torrent`). 
+/// each API-related method is listed as its category, followed by its name: `category` + `name_of_the_method`, in snake case. (eg: `torrents_add_torrent`).
 /// For more info about categories, look at the main  description of the library.
-/// 
-/// ## Methods 
+///
+/// ## Methods
 /// Practically all methods that return some kind of response, return a json one;
 /// so as a rule of thumb:
 /// - if the method ends with `raw`, it means it'll return the raw json [`String`] from the response.
@@ -27,19 +29,19 @@ pub struct QbitApi {
     pub(crate) cookie: Arc<RwLock<Cookie>>,
     pub(crate) reqwest_client: Client,
     credentials: Credentials,
-    cookie_hold: Arc<Mutex<bool>>
+    cookie_hold: Arc<Mutex<bool>>,
 }
 
 impl QbitApi {
     /// ## Usage
-    /// 
+    ///
     /// creates a new instance of [`QbitApi`].
-    /// 
+    ///
     /// ## Arguments
-    /// 
+    ///
     /// authority: the authority for the Qbittorrent WebUI API. eg: `"http://localhost:6011/"`
     /// credentials: the credentials to the account.
-    /// 
+    ///
     /// ## Example
     /// ```
     /// let qbit_api = QbitApi::new("http://localhost:6011/", Credentials::new("user_name", "password")).await.unwrap();
@@ -47,17 +49,27 @@ impl QbitApi {
     pub async fn new(authority: impl AsRef<str>, credentials: Credentials) -> Result<Self, Error> {
         let s = authority.as_ref();
         let authority = Into::<String>::into(s) as String;
-        let authority =  authority.chars().rev().skip_while(|s| *s as u8 == 47).clone().map(|k| k.to_string()).collect::<Vec<String>>().into_iter().rev().collect::<String>();
-        //println!("{}", authority);
+        let authority = authority
+            .chars()
+            .rev()
+            .skip_while(|s| *s as u8 == 47)
+            .clone()
+            .map(|k| k.to_string())
+            .collect::<Vec<String>>()
+            .into_iter()
+            .rev()
+            .collect::<String>();
         let reqwest_client = Client::new();
-        let cookie = Arc::new(RwLock::new(Cookie::new(&authority, &reqwest_client, &credentials).await?));
+        let cookie = Arc::new(RwLock::new(
+            Cookie::new(&authority, &reqwest_client, &credentials).await?,
+        ));
         return Ok(QbitApi {
             authority,
             cookie,
             reqwest_client,
             credentials,
-            cookie_hold: Arc::new(Mutex::new(false))
-        })
+            cookie_hold: Arc::new(Mutex::new(false)),
+        });
     }
 
     pub(crate) async fn get_cookie(&mut self) -> Result<String, Error> {
@@ -73,7 +85,9 @@ impl QbitApi {
                 drop(read_lock);
                 let cookie = self.cookie.clone();
                 let mut cookie = cookie.write().await;
-                cookie.reset(&self.authority, &self.reqwest_client, &self.credentials).await?;
+                cookie
+                    .reset(&self.authority, &self.reqwest_client, &self.credentials)
+                    .await?;
                 res = (&*cookie.cookie).to_string();
                 drop(cookie);
                 *hold_cookie = false;
@@ -88,53 +102,120 @@ impl QbitApi {
         Ok(res)
     }
 
-    pub(crate) async fn make_request<T: Into<String>, S: Into<String>>(&mut self, url: T, custom_error: S) -> Result<String, crate::Error> {
-        let resp = self.reqwest_client.post(format!("{}/api/v2{}", self.authority, url.into()))
+    pub(crate) async fn make_request<T: Into<String>, S: Into<String>>(
+        &mut self,
+        url: T,
+        custom_error: S,
+    ) -> Result<String, crate::Error> {
+        let resp = self
+            .reqwest_client
+            .post(format!("{}/api/v2{}", self.authority, url.into()))
             .header(COOKIE, format!("SID={}", self.get_cookie().await?))
-            .send().await.map_err(|e| Error::build(ErrorType::ReqwestError(Box::new(e)), None))?;
+            .send()
+            .await
+            .map_err(|e| Error::build(ErrorType::ReqwestError(Box::new(e)), None))?;
 
         if resp.status().is_success() {
-            let text = resp.text().await.map_err(|e| Error::build(ErrorType::ReqwestError(Box::new(e)), None))?;
+            let text = resp
+                .text()
+                .await
+                .map_err(|e| Error::build(ErrorType::ReqwestError(Box::new(e)), None))?;
 
             return Ok(text);
         } else {
-            return Err(Error::build(ErrorType::MiscError(format!("something went wrong. function name: {}", custom_error.into())), Some(resp.status().as_u16())));
+            return Err(Error::build(
+                ErrorType::MiscError(format!(
+                    "something went wrong. function name: {}",
+                    custom_error.into()
+                )),
+                Some(resp.status().as_u16()),
+            ));
         }
     }
 
-    pub(crate) async fn make_request_with_form<T: std::cmp::Eq + Hash + Serialize, S: Serialize, U, N: Into<String>, X: Into<String>>(&mut self, url: N, custom_error: X, hashmap: HashMap<T, S, U>) -> Result<String, crate::Error> {
-        let response = self.reqwest_client.post(format!("{}/api/v2{}", self.authority, url.into()))
-                .header(reqwest::header::COOKIE, format!("SID={}", self.get_cookie().await?))
-                .form(&hashmap)
-                .send()
+    pub(crate) async fn make_request_with_form<
+        T: std::cmp::Eq + Hash + Serialize,
+        S: Serialize,
+        U,
+        N: Into<String>,
+        X: Into<String>,
+    >(
+        &mut self,
+        url: N,
+        custom_error: X,
+        hashmap: HashMap<T, S, U>,
+    ) -> Result<String, crate::Error> {
+        let response = self
+            .reqwest_client
+            .post(format!("{}/api/v2{}", self.authority, url.into()))
+            .header(
+                reqwest::header::COOKIE,
+                format!("SID={}", self.get_cookie().await?),
+            )
+            .form(&hashmap)
+            .send()
+            .await
+            .map_err(|e| Error::build(ErrorType::ReqwestError(Box::new(e)), None))?;
+
+        if response.status().is_success() {
+            let text = response
+                .text()
                 .await
                 .map_err(|e| Error::build(ErrorType::ReqwestError(Box::new(e)), None))?;
-
-            if response.status().is_success() {
-                let text = response.text().await.map_err(|e| Error::build(ErrorType::ReqwestError(Box::new(e)), None))?;
-                Ok(text)
-            } else {
-                Err(Error::build(ErrorType::MiscError(format!("function name: {}", custom_error.into())), Some(response.status().as_u16())))
-            }
+            Ok(text)
+        } else {
+            Err(Error::build(
+                ErrorType::MiscError(format!("function name: {}", custom_error.into())),
+                Some(response.status().as_u16()),
+            ))
+        }
     }
 
-    pub(crate) async fn make_request_with_form_hash<T: std::cmp::Eq + Hash + Serialize, S: Serialize, U, N: Into<String>, X: Into<String>>(&mut self, url: N, custom_error: X, hashmap: HashMap<T, S, U>) -> Result<String, crate::Error> {
-        let response = self.reqwest_client.post(format!("{}/api/v2{}", self.authority, url.into()))
-                .header(reqwest::header::COOKIE, format!("SID={}", self.get_cookie().await?))
-                .form(&hashmap)
-                .send()
+    pub(crate) async fn make_request_with_form_hash<
+        T: std::cmp::Eq + Hash + Serialize,
+        S: Serialize,
+        U,
+        N: Into<String>,
+        X: Into<String>,
+    >(
+        &mut self,
+        url: N,
+        custom_error: X,
+        hashmap: HashMap<T, S, U>,
+    ) -> Result<String, crate::Error> {
+        let response = self
+            .reqwest_client
+            .post(format!("{}/api/v2{}", self.authority, url.into()))
+            .header(
+                reqwest::header::COOKIE,
+                format!("SID={}", self.get_cookie().await?),
+            )
+            .form(&hashmap)
+            .send()
+            .await
+            .map_err(|e| Error::build(ErrorType::ReqwestError(Box::new(e)), None))?;
+
+        // Handle the response
+        if response.status().is_success() {
+            let text = response
+                .text()
                 .await
                 .map_err(|e| Error::build(ErrorType::ReqwestError(Box::new(e)), None))?;
-
-            // Handle the response
-            if response.status().is_success() {
-                let text = response.text().await.map_err(|e| Error::build(ErrorType::ReqwestError(Box::new(e)), None))?;
-                Ok(text)
-            } else if response.status().as_u16() == 404 {
-                Err(Error::build(ErrorType::TorrentHashNotFound, code!(response)))
-            } else {
-                Err(Error::build(ErrorType::MiscError(format!("something went wrong. function name: {}", custom_error.into())), code!(response)))
-            }
+            Ok(text)
+        } else if response.status().as_u16() == 404 {
+            Err(Error::build(
+                ErrorType::TorrentHashNotFound,
+                code!(response),
+            ))
+        } else {
+            Err(Error::build(
+                ErrorType::MiscError(format!(
+                    "something went wrong. function name: {}",
+                    custom_error.into()
+                )),
+                code!(response),
+            ))
+        }
     }
 
     post_request_no_return!(logout, "/auth/logout");
--- qbittorrent_rust/src/core/cookie.rs
+++ qbittorrent_rust/src/core/cookie.rs
@@ -4,18 +4,23 @@ use reqwest::Client;
 
 use super::creds::Credentials;
 use crate::code;
-use crate::error_handling::errors::Error;
 use crate::error_handling::error_type::ErrorType;
+use crate::error_handling::errors::Error;
 
 #[derive(Debug, Clone)]
 pub(crate) struct Cookie {
     pub(crate) cookie: String,
     time_of_creation: Instant,
-} impl Cookie {
+}
+impl Cookie {
     /// makes a new instance of `Cookie`.
-    pub(crate) async fn new(authority: &String, reqwest_client: &Client, credentials: &Credentials) -> Result<Self, Error> {
+    pub(crate) async fn new(
+        authority: &String,
+        reqwest_client: &Client,
+        credentials: &Credentials,
+    ) -> Result<Self, Error> {
         let now = Instant::now();
-        
+
         let cookie = Self::request_raw_cookie(authority, reqwest_client, credentials).await?;
 
         Ok(Self {
@@ -24,11 +29,18 @@ pub(crate) struct Cookie {
         })
     }
 
-    pub(crate) async fn request_raw_cookie(authority: &String, reqwest_client: &Client, credentials: &Credentials) -> Result<String, Error> {
+    pub(crate) async fn request_raw_cookie(
+        authority: &String,
+        reqwest_client: &Client,
+        credentials: &Credentials,
+    ) -> Result<String, Error> {
         let response = reqwest_client
             .post(format!("{}/api/v2/auth/login", authority))
             .header(reqwest::header::REFERER, authority)
-            .form(&[("username", credentials.username.clone()), ("password", credentials.password.clone())])
+            .form(&[
+                ("username", credentials.username.clone()),
+                ("password", credentials.password.clone()),
+            ])
             .send()
             .await
             .map_err(|e| Error::build(ErrorType::ReqwestError(Box::new(e)), None))?;
@@ -36,28 +48,40 @@ pub(crate) struct Cookie {
         let status = response.status();
 
         if status.is_success() {
-            match response.headers().get("set-cookie").and_then(|s| s.to_str().ok()) {
+            match response
+                .headers()
+                .get("set-cookie")
+                .and_then(|s| s.to_str().ok())
+            {
                 Some(cookie) => {
-                    println!("{}", cookie);
-                    return Ok(cookie.to_string().split("=").collect::<Vec<&str>>()[1].split(";").collect::<Vec<&str>>()[0].to_string());
-                },
-
-                None => {
-                    return Err(Error::build(ErrorType::WrongCreds, None))
+                    return Ok(cookie.to_string().split("=").collect::<Vec<&str>>()[1]
+                        .split(";")
+                        .collect::<Vec<&str>>()[0]
+                        .to_string());
                 }
+
+                None => return Err(Error::build(ErrorType::WrongCreds, None)),
             }
         } else if status.as_u16() == 403 {
-            return Err(Error::build(ErrorType::TooManyFailedAttempts, Some(403_u16)));
+            return Err(Error::build(
+                ErrorType::TooManyFailedAttempts,
+                Some(403_u16),
+            ));
         } else {
-            return Err(Error::build(ErrorType::MiscError("Something went wrong while getting the auth cookie.".to_string()), code!(response)))
+            return Err(Error::build(
+                ErrorType::MiscError(
+                    "Something went wrong while getting the auth cookie.".to_string(),
+                ),
+                code!(response),
+            ));
         }
     }
-    
+
     /// checks if the `Cookie` expired.
-    /// 
+    ///
     /// # FUNCTIONING
     /// returns `true` when the cookie is expired.
-    /// 
+    ///
     /// # WARNING
     /// - this method relies on the elapsed seconds from the time of creation. make sure to something that messes with that.
     /// - this will check that there is at least 1 minute of margin from the official expiration, to ensure any operation after this methis is called is still possible.
@@ -70,10 +94,15 @@ pub(crate) struct Cookie {
     }
 
     /// checks if the `Cookie` is expired, and if it is, requests a new one.
-    pub(crate) async fn reset(&mut self, authority: &String, reqwest_client: &Client, credentials: &Credentials) -> Result<(), Error> {
+    pub(crate) async fn reset(
+        &mut self,
+        authority: &String,
+        reqwest_client: &Client,
+        credentials: &Credentials,
+    ) -> Result<(), Error> {
         if self.is_expired() {
             self.cookie = Self::request_raw_cookie(authority, reqwest_client, credentials).await?;
-        } 
+        }
 
         Ok(())
     }
